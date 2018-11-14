f9e223e9a1777118a9bf07af468916808067bc83
shota-nakagawagit config -global user.email asteriskdanmach@yahoo.co.jpgit config --global user.name shota-nakagawa <root@LAPTOP-281C818U.localdomain>
2018-11-14 14:42:05 +0900

    XXXx

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
 
-  resources :users
+   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
