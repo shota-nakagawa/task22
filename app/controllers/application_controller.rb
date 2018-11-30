286aa343873d67b16a1a60838888038e4985fb20
shota-nakagawa <44248146+shota-nakagawa@users.noreply.github.com>
2018-11-30 12:55:46 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :login_check, only:[:new, :edit, :update, :destroy]
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
 
